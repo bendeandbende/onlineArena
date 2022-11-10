fea55122f0bbd01e5020396229380001055905c9
bendeandbende <tomivalmosok@gmail.com>
2022-11-10 17:45:40 +0100

    hotfix

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(helmet());
 app.use(
   cors({
-    origin: 'https://bb-onlinearena.netlify.app',
+    origin: '*',
   })
 );
 app.use(express.json({ limit: '10kb' }));
